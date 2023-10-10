d195fb2fc5892e454fc289007c522859b31f622f
Kb-Jr <abdulkabiruj@gmail.com>
2023-10-10 19:59:51 +0100

    Ecommerce Page
    Not completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,24 +15,44 @@ import { useStateContext } from '../contexts/ContextProvider';
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
   <TooltipComponent content={title} position='BottomCenter'>
     <button type='button' onClick={customFunc} style={{color}} className='relative rounded-full text-xl hover:bg-light-gray p-3 '>
-      <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>{icon}</span>
+      <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'/>
+      {icon}
     </button>
   </TooltipComponent>
 )
 
 const Navbar = () => {
 
-  const {activeMenu, setactiveMenu} = useStateContext();
+  const {activeMenu, setactiveMenu, isClicked, setisClicked, handleClick, screenSize, setscreenSize} = useStateContext();
+
+  useEffect(() => {
+    const handleResize = () => setscreenSize(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, []);
+
+
+  useEffect(() => {
+    if(screenSize <= 900) {
+      setactiveMenu(false);
+    }
+    else{setactiveMenu(true);
+    }
+
+  }, [screenSize]);
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative' >
       <NavButton title="Menu" customFunc={() => setactiveMenu((prevactiveMenu)=> !prevactiveMenu)} color='blue' icon={<AiOutlineMenu/>}/>
      
       <div className='flex'>
-        <NavButton title="Cart" color='blue' icon={<FiShoppingCart/>}/>
-        <NavButton title="Chat" dotColor='#03C9D7' color='blue' icon={<BsChatLeft/>}/>
-        <NavButton title="Notification" dotColor='#03C9D7' color='blue' icon={<RiNotification3Line/>}/>
-        <TooltipComponent content="Profile" position='BottomCenter'>
+        <NavButton title="Cart" color='blue' customFunc={() => handleClick('cart')} icon={<FiShoppingCart/>}/>
+        <NavButton title="Chat" dotColor='#03C9D7' customFunc={() => handleClick('chat')} color='blue' icon={<BsChatLeft/>}/>
+        <NavButton title="Notification" dotColor='#03C9D7' color='blue' customFunc={() => handleClick('notification')} icon={<RiNotification3Line/>}/>
+        <TooltipComponent content="Profile" position='BottomCenter' onClick={() => handleClick('userProfile')}>
           <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'>
             <img src={avatar} className='rounded-full w-8 h-8'/>
             <p>
@@ -43,6 +63,10 @@ const Navbar = () => {
             
           </div>
         </TooltipComponent>
+        {isClicked.cart && <Cart/>}
+        {isClicked.chat && <Chat/>}
+        {isClicked.notification && <Notifications/>}
+        {isClicked.userProfile && <UserProfile/>}
       </div>
 
     </div>
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -9,11 +9,15 @@ const initialState = {
     notification : false
 }
 
-
 export const ContextProvider = ({children}) => {
     const [activeMenu, setactiveMenu] = useState(true);
+    const [isClicked, setisClicked] = useState(initialState);
+    const handleClick = (clicked) => {
+        setisClicked({...initialState, [clicked]:true});
+    }
+    const [screenSize, setscreenSize] = useState(undefined);
     return (
-        <StateContext.Provider value={{activeMenu, setactiveMenu}}>
+        <StateContext.Provider value={{activeMenu, setactiveMenu, isClicked,setisClicked, handleClick, screenSize, setscreenSize}}>
             {children}
         </StateContext.Provider>
     )
