0c282db87e74507cf1c6577845246f34ce9bf168
Kb-Jr <abdulkabiruj@gmail.com>
2023-10-12 16:23:30 +0100

    Update ColorPicker.jsx

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs'
+
+import { Header } from '../components'
+
+const Change = (args) => {
+  document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+}
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className='m-2 md:m-10 mt-24 p-2 rounded-3xl md:p-10 bg-white'>
+      <Header category="App" title="Color Picker"/>
+        <div className='text-center'>
+          <div id='preview'></div>
+          <div className='flex justify-center items-center flex-wrap gap-20'>
+            <div>
+              <p className='text-2xl font-semibold mt-2 mb-4'>Inline Pallete</p>
+              <ColorPickerComponent id='inline pallete' mode='Pallete' modeSwitcher={false} inline showButtons={false} change={Change}/>
+            </div>
+            <div>
+              <p className='text-2xl font-semibold mt-2 mb-4'>Inline Picker</p>
+              <ColorPickerComponent id='inline pallete' mode='Picker' modeSwitcher={false} inline showButtons={false} change={Change}/>
+            </div>
+            
+          </div>
+
+        </div>
+
+        
+
+  </div>
   )
 }
 
