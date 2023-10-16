67cc241f86d3189d291d08c394cd8897bbe40f25
Kb-Jr <abdulkabiruj@gmail.com>
2023-10-16 11:12:41 +0100

    Pie Chart

--- src/components/Charts/PieChart.jsx
+++ src/components/Charts/PieChart.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip, Inject } from '@syncfusion/ej2-react-charts';
+import * as ReactDOM from "react-dom";
+import { pieChartData } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
 
 const PieChart = () => {
+const {currentMode} = useStateContext();
+let pie;
+
   return (
-    <div>PieChart</div>
+    <AccumulationChartComponent id='pie-chart' ref={pie => pie = pie} legendSettings={{
+      visible: true
+  }} enableSmartLabels={true} enableAnimation={true} tooltip={{ enable: true }} background={currentMode === 'Dark' ? '#fff' : '#fff'}>
+      <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]}/>
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective dataSource={pieChartData} xName='x' yName='y' innerRadius='20%' dataLabel={{
+      visible: true, position: 'Outside', name: 'x'
+  }} radius='r'>
+        </AccumulationSeriesDirective>
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
   )
 }
 
